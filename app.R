068714f22ca96e40a0a472245c9df041a6ec2ccc
palcape <calvinpape@gmail.com>
2018-10-17 11:53:20 -0600

    Cleaning up code

--- app.R
+++ app.R
@@ -15,19 +15,20 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                       on the Category Bar at the top of the screen to see different categories of data. Once you've
                                       found a plot you like, you can use its interactive features to explore your data. Double click a series
                                       on the legend to isolate the plot to that one data series.")
-                           ),
+                           ),       
                            tabPanel("Program Overview",
-                                      h4("Program Overview"),
-                                        plotlyOutput("ProgramOverviewPlot")
-                           ),
+                                    h3("Program Overview"),
+                                        plotlyOutput("POplot")
+                           ),       
                            tabPanel("Client Demographics",        
+                                    h3("Client Demographics"),
                                       h4("Gender"),
                                         plotlyOutput("GenderPlot"),
                                       h4("Age"),
                                         plotlyOutput("AgesLinePlot"),
                                       h4("Race/Ethnicity"),
                                         plotlyOutput("RaceLinePlot")
-                           ),
+                           ),       
                            tabPanel("Referrals and Enrollments",
                                     h3("Referrals and Enrollments"),
                                       h4("Eligible per HMIS Data Pull"),
@@ -36,17 +37,16 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                         plotlyOutput("TRHplot"),
                                       h4("Prescreen Eligibility"),
                                         plotlyOutput("PrescreenPlot")
-                           ), 
+                           ),       
                            tabPanel("Housing Placement and Services",
                                     h3("Housing Placement and Services"),
                                       h4("Roommate Placement Assessments Conducted"),
                                         plotlyOutput("RPACplot"),
-                                    h3("Placements into Housing"),
                                       h4("New Placements into Housing"),
                                         plotlyOutput("NPiHplot"),
                                       h4("Awaiting Housing"),
                                         plotlyOutput("AHplot")
-                           ),
+                           ),       
                            tabPanel("Behavioral Health",
                                     h3("Behavioral Health"),
                                       h4("Referrals"),
@@ -55,18 +55,18 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                         plotlyOutput("CSplot"),
                                       h4("Enrolled in Additional Community Services"),
                                         plotlyOutput("EiACSplot")
-                           ),
+                           ),         
                            tabPanel("Employment",
                                     h3("Employment"),
                                         plotlyOutput("EmploymentPlot")
-                           ),
+                           ),         
                            tabPanel("Staffing",
                                     h3("Staffing"),
                                       h4("Staff to Client Ratio"),
                                         plotlyOutput("StCRplot"),
                                       h4("Staffing Positions"),
                                         plotlyOutput("SPplot")
-                           ),
+                           ),       
                            tabPanel("Exits",
                                     h3("Exits"),
                                       h4("Clients that Lost Housing"),
@@ -77,7 +77,7 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                         plotlyOutput("PGplot")
                            )
                 ),
-                
+
                 HTML('<center><img src="footer.jpg"></center>')
 )
 # Define Server
@@ -116,90 +116,83 @@ server <- function(input, output) {
   ## Graphics
     ### Program Overview
       #### Program Overview
-        output$ProgramOverviewPlot <- renderPlotly({ProgramOverviewPlot <- plot_ly(x = months, y = strtoi(tData[ ,1]), name = 'New Clients', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[ ,2]), name = 'Total Clients', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,3]), name = 'Clients Housed', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,4]), name = 'Clients Permanently Housed', mode = 'lines+markers') %>%
-          layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = ax)
-        })
+      output$POplot <- renderPlotly({POplot <- plot_ly(x = months, y = strtoi(tData[ ,1]), name = 'New Clients', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[ ,2]), name = 'Total Clients', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,3]), name = 'Clients Housed', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,4]), name = 'Clients Permanently Housed', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = ax)
+      })
     ### Client Demographics
       #### Gender
-        output$GenderPlot <- renderPlotly({GenderPlot <- plot_ly(x = months, y = strtoi(tData[ ,5]), name = 'Male', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[ ,6]), name = 'Female', mode = 'lines+markers')%>%
-          layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
+      output$GenderPlot <- renderPlotly({GenderPlot <- plot_ly(x = months, y = strtoi(tData[ ,5]), name = 'Male', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[ ,6]), name = 'Female', mode = 'lines+markers')%>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
       #### Age 
-        output$AgesLinePlot <- renderPlotly({AgesLinePlot <- plot_ly(x = months, y = strtoi(tData[ ,9]), name = '26-35', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[ ,10]), name = '36-45', mode = 'lines+markers')%>%
-          add_trace(y = strtoi(tData[ ,11]), name = '45+', mode = 'lines+markers')%>%
-          layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
+      output$AgesLinePlot <- renderPlotly({AgesLinePlot <- plot_ly(x = months, y = strtoi(tData[ ,9]), name = '26-35', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[ ,10]), name = '36-45', mode = 'lines+markers')%>%
+        add_trace(y = strtoi(tData[ ,11]), name = '45+', mode = 'lines+markers')%>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
       #### Race
-        output$RaceLinePlot <- renderPlotly({ RaceLinePlot <- plot_ly(x = months, y = strtoi(tData[ ,12]), name = 'American Indian', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[,13]), name = 'Asian', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[,14]), name = 'Pacific Islander', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[,15]), name = 'Black/African American', mode = 'lines+markers')%>%
-          add_trace(y = strtoi(tData[,16]), name = 'White', mode = 'lines+markers')%>%
-          add_trace(y = strtoi(tData[,17]), name = 'Other: Two or more races', mode = 'lines+markers')%>%
-          add_trace(y = strtoi(tData[,20]), name = 'Hispanic', mode = 'lines+markers')%>%
-          add_trace(y = strtoi(tData[,21]), name = 'Other: Non-Hispanic', mode = 'lines+markers')%>%
-          layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
+      output$RaceLinePlot <- renderPlotly({ RaceLinePlot <- plot_ly(x = months, y = strtoi(tData[ ,12]), name = 'American Indian', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[,13]), name = 'Asian', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[,14]), name = 'Pacific Islander', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[,15]), name = 'Black/African American', mode = 'lines+markers')%>%
+        add_trace(y = strtoi(tData[,16]), name = 'White', mode = 'lines+markers')%>%
+        add_trace(y = strtoi(tData[,17]), name = 'Other: Two or more races', mode = 'lines+markers')%>%
+        add_trace(y = strtoi(tData[,20]), name = 'Hispanic', mode = 'lines+markers')%>%
+        add_trace(y = strtoi(tData[,21]), name = 'Other: Non-Hispanic', mode = 'lines+markers')%>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
     ### Referrals and Enrollment 
       #### Eligible per HMIS Data Pull
-        ##### Eligible per HMIS Data Pull
-        output$HMISplot <- renderPlotly({HMISplot <- plot_ly(x = months, y = strtoi(tData[,23]), type = 'bar', name = 'Eligible per HMIS Data Pull') %>%
-        layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
+      output$HMISplot <- renderPlotly({HMISplot <- plot_ly(x = months, y = strtoi(tData[,23]), type = 'bar', name = 'Eligible per HMIS Data Pull') %>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
       #### TRH Location Endeavors
-        ##### TRH Location Endeavors
-        output$TRHplot <- renderPlotly({TRHplot <- plot_ly(x = months, y = strtoi(tData[ ,24]), name = 'Attempted to Locate', type = 'scatter', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,25]), name = 'Located and Prescreened', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
-        })
+      output$TRHplot <- renderPlotly({TRHplot <- plot_ly(x = months, y = strtoi(tData[ ,24]), name = 'Attempted to Locate', type = 'scatter', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,25]), name = 'Located and Prescreened', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
+      })
       #### Prescreen Eligibility
-        ##### Eligible for Prescreen
-        output$PrescreenPlot <- renderPlotly({PrescreenPlot <- plot_ly(x = months, y = strtoi(tData[ ,33]), name = 'Eligible after Prescreen', type = 'scatter', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,34]), name = 'Ineligible after Prescreen', mode = 'lines+markers') %>%
-          layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
-        })
-        
+      output$PrescreenPlot <- renderPlotly({PrescreenPlot <- plot_ly(x = months, y = strtoi(tData[ ,33]), name = 'Eligible after Prescreen', type = 'scatter', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,34]), name = 'Ineligible after Prescreen', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
+      })
     ### Housing Placement and Services
       #### Roommate Placement Assessments Conducted
-        ##### Roommate Placement Assessments Conducted
-        output$RPACplot <- renderPlotly({RPACplot <- plot_ly(x = months, y = strtoi(tData[,49]), type = 'bar', name = 'Roommate Placement Assessments Conducted')  %>%
-        layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
-      #### Placements into Housing
-        ##### New Placements into Housing
-        output$NPiHplot <- renderPlotly({NPiHplot <- plot_ly(x = months, y = strtoi(tData[ ,50]), name = 'Total', type = 'scatter', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,51]), name = 'Single Occupancy Housing', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,52]), name = 'Roommate Housing', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
-        })
-        ##### Awaiting Housing
-        output$AHplot <- renderPlotly({AHplot <- plot_ly(x = months, y = strtoi(tData[ ,53]), name = 'Awaiting Housing Placement', type = 'scatter', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[ ,54]), name = 'Not Housed within Three Months of Enrollment', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
-        })
+      output$RPACplot <- renderPlotly({RPACplot <- plot_ly(x = months, y = strtoi(tData[,49]), type = 'bar', name = 'Roommate Placement Assessments Conducted')  %>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
+      #### New Placements into Housing
+      output$NPiHplot <- renderPlotly({NPiHplot <- plot_ly(x = months, y = strtoi(tData[ ,50]), name = 'Total', type = 'scatter', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,51]), name = 'Single Occupancy Housing', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,52]), name = 'Roommate Housing', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
+      })
+      #### Awaiting Housing
+      output$AHplot <- renderPlotly({AHplot <- plot_ly(x = months, y = strtoi(tData[ ,53]), name = 'Awaiting Housing Placement', type = 'scatter', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[ ,54]), name = 'Not Housed within Three Months of Enrollment', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals'), xaxis = list(title = 'Month'))
+      })
     ### Behavioral Health
-      #### Behavioral Health
-        #### Referrals
-        output$ReferralsPlot <- renderPlotly({ReferralsPlot <- plot_ly(x = months, y = strtoi(tData[,60]), name = 'Referred to BH Treatment', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[,62]), name = 'Referred to BH Treatment after HNJ Assessment ', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
-        #### HNJ Clinician Statistics
-        output$CSplot <- renderPlotly({CSplot <- plot_ly(x = months, y = strtoi(tData[,61]), name = 'HNJ Health Assessments Conducted', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[,63]), name = 'Met with HNJ Clinician', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
-        #### Enrolled in Additional Community Services
-        output$EiACSplot <- renderPlotly({EiACSplot <- plot_ly(x = months, y = strtoi(tData[,64]), name = 'Total', type = 'scatter', mode = 'lines+markers')  %>%
-          add_trace(y = strtoi(tData[,65]), name = 'SUD', mode = 'lines+markers') %>%
-          add_trace(y = strtoi(tData[,66]), name = 'Behavioral', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
-        })
+      #### Referrals
+      output$ReferralsPlot <- renderPlotly({ReferralsPlot <- plot_ly(x = months, y = strtoi(tData[,60]), name = 'Referred to BH Treatment', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[,62]), name = 'Referred to BH Treatment after HNJ Assessment ', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
+      #### HNJ Clinician Statistics
+      output$CSplot <- renderPlotly({CSplot <- plot_ly(x = months, y = strtoi(tData[,61]), name = 'HNJ Health Assessments Conducted', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[,63]), name = 'Met with HNJ Clinician', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
+      #### Enrolled in Additional Community Services
+      output$EiACSplot <- renderPlotly({EiACSplot <- plot_ly(x = months, y = strtoi(tData[,64]), name = 'Total', type = 'scatter', mode = 'lines+markers')  %>%
+        add_trace(y = strtoi(tData[,65]), name = 'SUD', mode = 'lines+markers') %>%
+        add_trace(y = strtoi(tData[,66]), name = 'Behavioral', mode = 'lines+markers') %>%
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month'))
+      })
     ### Employment
       #### Employment  
       output$EmploymentPlot <- renderPlotly({EmploymentPlot <- plot_ly(x = months, y = strtoi(tData[,67]), name = 'Employment Assessments Conducted', type = 'scatter', mode = 'lines+markers')  %>%
@@ -216,7 +209,7 @@ server <- function(input, output) {
       #### Staffing Positions
       output$SPplot <- renderPlotly({SPplot <- plot_ly(x = months, y = strtoi(tData[,86]), name = 'Positions Available', type = 'scatter', mode = 'lines+markers')  %>%
         add_trace(y = strtoi(tData[,87]), name = 'Positions Filled', mode = 'lines+markers') %>%
-        layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month', ax))
+      layout(yaxis = list(title = 'Number of Individuals', rangemode = "tozero"), xaxis = list(title = 'Month', ax))
       })
     ### Exits
       #### Clients that Lost Housing this Month
